2527bd220b9670cfd350eb4c1a6ec2432b0cf5d4
steveyu <littlepageprogram@163.com>
2021-01-20 02:31:02 +0800

    test

---
+++ practice/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+struct node {
+    int value;
+    node *left, *right;
+    node(int value) : value(value), left(NULL), right(NULL) {}
+};
+int main(int argc, char **argv) {
+    return 0;
+}
